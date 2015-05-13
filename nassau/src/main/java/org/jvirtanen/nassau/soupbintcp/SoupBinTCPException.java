4cc1f8e44923d1b4f61392ee851c2c8e332eeca3
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-13 09:49:13 +0300

    Add documentation for SoupBinTCP exception

--- nassau/src/main/java/org/jvirtanen/nassau/soupbintcp/SoupBinTCPException.java
+++ nassau/src/main/java/org/jvirtanen/nassau/soupbintcp/SoupBinTCPException.java
@@ -2,8 +2,16 @@ package org.jvirtanen.nassau.soupbintcp;
 
 import org.jvirtanen.nassau.ProtocolException;
 
+/**
+ * Indicates a protocol error while handling the SoupBinTCP protocol.
+ */
 public class SoupBinTCPException extends ProtocolException {
 
+    /**
+     * Create an instance with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public SoupBinTCPException(String message) {
         super(message);
     }
