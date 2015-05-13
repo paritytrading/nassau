c4af7222c9d2c560c3cfd83c3cb18b1ec6cb452d
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-13 09:50:52 +0300

    Clean up interface for system clock

--- nassau/src/main/java/org/jvirtanen/nassau/util/SystemClock.java
+++ nassau/src/main/java/org/jvirtanen/nassau/util/SystemClock.java
@@ -4,6 +4,9 @@ public class SystemClock implements Clock {
 
     public static final SystemClock INSTANCE = new SystemClock();
 
+    private SystemClock() {
+    }
+
     @Override
     public long currentTimeMillis() {
         return System.currentTimeMillis();
